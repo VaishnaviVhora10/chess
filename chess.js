3bb23ed36b0525241fef4079ace8607bde78aca1
vaishnavivhora <86514711+VaishnaviVhora10@users.noreply.github.com>
2022-03-19 12:58:41 +0530

    logic for the chess board is created

---
+++ chess.js
@@ -0,0 +1,137 @@
+let a = [
+    ['A1:', "white rook"],
+    ['A2:', "white pawn"], 
+    ['A3', "Empty"],
+    ['A4', "Empty"],
+    ['A5', "Empty"],
+    ['A6', "Empty"],
+    ['A7', "black pawn"],
+    ['A8', "black rook"],
+    ];
+    for(let i = 0; i < a.length; i++){    
+        let inneraLength = a[i].length;
+    
+        for(let j = 0; j < inneraLength; j++) {
+            console.log(a[i][j]);
+        }
+    }
+    let b = [
+        ['B1:', "white knight"],
+        ['B2:', "wwhite pawn"], 
+        ['B3', "Empty"],
+        ['B4', "Empty"],
+        ['B5', "Empty"],
+        ['B6', "Empty"],
+        ['B7', "black pawn"],
+        ['B8', "black knight"],
+        ];
+        for(let i = 0; i < b.length; i++){    
+            let innerbLength = b[i].length;
+        
+            for(let j = 0; j < innerbLength; j++) {
+                console.log(b[i][j]);
+            }
+        }
+        let c = [
+            ['C1:', "white bishop"],
+            ['C2:', "wwhite pawn"], 
+            ['C3', "Empty"],
+            ['C4', "Empty"],
+            ['C5', "Empty"],
+            ['C6', "Empty"],
+            ['C7', "black pawn"],
+            ['C8', "black bishop"],
+            ];
+            for(let i = 0; i < c.length; i++){    
+                let innercLength = c[i].length;
+            
+                for(let j = 0; j < innercLength; j++) {
+                    console.log(c[i][j]);
+                }
+            }
+            let d = [
+                ['D1:', "white queen"],
+                ['D2:', "wwhite pawn"], 
+                ['D3', "Empty"],
+                ['D4', "Empty"],
+                ['D5', "Empty"],
+                ['D6', "Empty"],
+                ['D7', "black pawn"],
+                ['D8', "black queen"],
+                ];
+                for(let i = 0; i < d.length; i++){    
+                    let innerdLength = d[i].length;
+                
+                    for(let j = 0; j < innerdLength; j++) {
+                        console.log(d[i][j]);
+                    }
+                }
+                let e = [
+                    ['E1:', "white king"],
+                    ['E2:', "wwhite pawn"], 
+                    ['E3', "Empty"],
+                    ['E4', "Empty"],
+                    ['E5', "Empty"],
+                    ['E6', "Empty"],
+                    ['E7', "black pawn"],
+                    ['E8', "black king"],
+                    ];
+                    for(let i = 0; i < e.length; i++){    
+                        let innereLength = e[i].length;
+                    
+                        for(let j = 0; j < innereLength; j++) {
+                            console.log(e[i][j]);
+                        }
+                    }
+                    let f = [
+                        ['F1:', "white bishop"],
+                        ['F2:', "wwhite pawn"], 
+                        ['F3', "Empty"],
+                        ['F4', "Empty"],
+                        ['F5', "Empty"],
+                        ['F6', "Empty"],
+                        ['F7', "black pawn"],
+                        ['F8', "black bishop"],
+                        ];
+                        for(let i = 0; i < f.length; i++){    
+                            let innerfLength = f[i].length;
+                        
+                            for(let j = 0; j < innerfLength; j++) {
+                                console.log(f[i][j]);
+                            }
+                        }
+                        let g = [
+                            ['G1:', "white knight"],
+                            ['G2:', "wwhite pawn"], 
+                            ['G3', "Empty"],
+                            ['G4', "Empty"],
+                            ['G5', "Empty"],
+                            ['G6', "Empty"],
+                            ['G7', "black pawn"],
+                            ['G8', "black knight"],
+                            ];
+                            for(let i = 0; i < g.length; i++){    
+                                let innergLength = g[i].length;
+                            
+                                for(let j = 0; j < innergLength; j++) {
+                                    console.log(g[i][j]);
+                                }
+                            }
+                            let h = [
+                                ['H1:', "white rook"],
+                                ['H2:', "wwhite pawn"], 
+                                ['H3', "Empty"],
+                                ['H4', "Empty"],
+                                ['H5', "Empty"],
+                                ['H6', "Empty"],
+                                ['H7', "black pawn"],
+                                ['H8', "black rook"],
+                                ];
+                                
+                                for(let i = 0; i < h.length; i++){    
+                                    let innerhLength = h[i].length;
+                                
+                                    for(let j = 0; j < innerhLength; j++) {
+                                        console.log(h[i][j]);
+                                    }
+                                }
